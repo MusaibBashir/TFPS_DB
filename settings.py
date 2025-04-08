2c2ac9d5cdec0a08ed595203445df1ec4842f4f5
Musaib Bin Bashir <musaibbashir02@gmail.com>
2025-04-09 01:35:38 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,5 @@
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tfps_db',
-        'USER': 'your_db_user',
-        'PASSWORD': 'oeNzOAKPmaXqUG1AH2UqggoFoSe3Mx5F',
-        'HOST': 'dpg-cvqk52a4d50c7397lobg-a',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
