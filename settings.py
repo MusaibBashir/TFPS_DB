da98370d99f6b9e2697f68e665b9f705e7ec00bd
Musaib Bin Bashir <musaibbashir02@gmail.com>
2025-04-09 01:32:20 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
+        'NAME': 'tfps_db',
         'USER': 'your_db_user',
-        'PASSWORD': 'your_password',
-        'HOST': 'your_db_host',
+        'PASSWORD': 'oeNzOAKPmaXqUG1AH2UqggoFoSe3Mx5F',
+        'HOST': 'dpg-cvqk52a4d50c7397lobg-a',
         'PORT': '5432',
     }
 }
